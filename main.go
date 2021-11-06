a77878f32c68116e51f9604d8a7115da1484be92
Mohammad <mohamadsamanipoor@yahoo.com>
2021-11-06 23:25:26 +0330

    Multiple check in second

--- main.go
+++ main.go
@@ -3,25 +3,35 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 	links := []string{
 		"http://google.com",
-		"http://facebook.com",
-		"http:vadaa.com",
 		"http://vadaad.com",
+		"http://vadaad.ir",
 	}
 
+	c := make(chan string)
 	for _, link := range links {
-		checkStatus(link)
+		go checkStatus(link, c)
+	}
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkStatus(link, c)
+		}(l)
 	}
 }
 
-func checkStatus(l string) {
+func checkStatus(l string, c chan string) {
 	_, err := http.Get(l)
 	if err != nil {
 		fmt.Println("Site", l, "maybe down !")
+		c <- l
+		return
 	}
 	fmt.Println("Site", l, "is up !")
+	c <- l
 }
